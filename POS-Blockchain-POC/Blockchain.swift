17b6b3d34cc60543991828a085ec9d663e134564
Jonathan Oliveira <jonathan@jonathanoliveira.org>
2019-06-22 17:23:44 -0400

    blockchain basic elements

---
+++ POS-Blockchain-POC/Blockchain.swift
@@ -0,0 +1,13 @@
+//
+//  Blockchain.swift
+//  POS-Blockchain-POC
+//
+//  Created by Jonathan Oliveira on 22/06/19.
+//  Copyright © 2019 Jonathan Oliveira. All rights reserved.
+//
+
+import Foundation
+
+struct Blockchain {
+	var chain = [Block]()
+}
---
+++ POS-Blockchain-POC/Transaction.swift
@@ -0,0 +1,11 @@
+//
+//  Transaction.swift
+//  POS-Blockchain-POC
+//
+//  Created by Jonathan Oliveira on 22/06/19.
+//  Copyright © 2019 Jonathan Oliveira. All rights reserved.
+//
+
+import Foundation
+
+protocol Transaction {}
