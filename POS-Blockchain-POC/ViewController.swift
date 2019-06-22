3163267d0d276d4423524f122cb546ae4c25b576
Jonathan Oliveira <jonathan@jonathanoliveira.org>
2019-06-22 16:43:41 -0400

    initial commit

---
+++ POS-Blockchain-POC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POS-Blockchain-POC
+//
+//  Created by Jonathan Oliveira on 22/06/19.
+//  Copyright © 2019 Jonathan Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ POS-Blockchain-POCTests/POS_Blockchain_POCTests.swift
@@ -0,0 +1,34 @@
+//
+//  POS_Blockchain_POCTests.swift
+//  POS-Blockchain-POCTests
+//
+//  Created by Jonathan Oliveira on 22/06/19.
+//  Copyright © 2019 Jonathan Oliveira. All rights reserved.
+//
+
+import XCTest
+@testable import POS_Blockchain_POC
+
+class POS_Blockchain_POCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
