33212605d25b62e9501a4fec8f570b0b8216a6f2
Jonathan Oliveira <jonathan@jonathanoliveira.org>
2019-06-22 16:58:56 -0400

    create block struct

---
+++ POS-Blockchain-POC/Block.swift
@@ -0,0 +1,18 @@
+//
+//  Block.swift
+//  POS-Blockchain-POC
+//
+//  Created by Jonathan Oliveira on 22/06/19.
+//  Copyright © 2019 Jonathan Oliveira. All rights reserved.
+//
+
+import Foundation
+
+struct Block {
+	let hash: String
+	let previousBlockHash: String
+	
+	func generateHash() -> String {
+		return NSUUID().uuidString.replacingOccurrences(of: "-", with: "")
+	}
+}
